58d797b95ca6a109ce36f91c6b917b7ae455a580
mariacjones1 <mariacjones1@hotmail.co.uk>
2023-02-22 11:29:33 +0000

    Create function allowing user to create username

--- run.py
+++ run.py
@@ -4,13 +4,13 @@ def start_game():
     and gives user the opportunity to start the game
     """
     print("Welcome to Battleships!\n"
-           "The aim of the game is to find and sink your opponent's"
-           "battleships before they sink yours.\n"
-           "Choose the location for each of your ships, or generate"
-           "random positions, then take it in turns to guess"
-           "where each other's ships are.\n"
-           "Ready to play?")
-    
+          "The aim of the game is to find and sink your opponent's"
+          "battleships before they sink yours.\n"
+          "Choose the location for each of your ships, or generate"
+          "random positions, then take it in turns to guess"
+          "where each other's ships are.\n"
+          "Ready to play?")
+
     while True:
         user_start = input("Y/N\n").upper()
 
@@ -25,4 +25,31 @@ def start_game():
     return user_start
 
 
+def create_user():
+    """
+    Gets the user to enter a username
+    Must be at least three characters and contain
+    alphanumeric characters only or an error will be raised
+    """
+    print("Excellent! Before we get started, what should we call you?")
+
+    while True:
+        username = input("Enter username: ")
+
+        try:
+            if username.isalnum() is False:
+                raise TypeError
+            elif len(username) < 3:
+                raise ValueError
+        except TypeError:
+            print("Username should use numbers and letters only.")
+        except ValueError:
+            print("Username must be at least 3 characters.")
+        else:
+            break
+
+    return username
+
+
 start_game()
+create_user()
